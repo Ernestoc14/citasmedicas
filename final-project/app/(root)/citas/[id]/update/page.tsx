b01bc3f1a406f2031ee14ccf0a0bfd4a0bdbe72f
Ernestoc14 <descreed34@hotmail.com>
2024-11-16 23:00:56 -0500

    Feat: Adding a URL to better understanding of update specific cita route

--- final-project/app/(root)/citas/[id]/update/page.tsx
+++ final-project/app/(root)/citas/[id]/update/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-// To Update a Specific Cita
+// To Update a Specific Cita 
+// http://localhost:3000/citas/12/update
 const UpdateCitaPage = () => {
   return (
     <div>UpdateCitaPage</div>
